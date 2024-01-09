837636de702b07a2d7f7b6848279f79c98596a9d
Samra Abdurahmanli <samra.abdurahmanli.a@asoiu.edu.az>
2024-01-09 11:46:43 +0400

    camera permission

--- app/src/main/java/com/samra/facedetection/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/samra/facedetection/ui/camera/CameraFragment.kt
@@ -1,16 +1,34 @@
 package com.samra.facedetection.ui.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.samra.facedetection.databinding.FragmentCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
 class CameraFragment : Fragment() {
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var cameraExecutor: ExecutorService
 
+    companion object {
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,5 +37,74 @@ class CameraFragment : Fragment() {
         binding = FragmentCameraBinding.inflate(layoutInflater , container , false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        permissionHandler()
+        startCamera(binding.cameraPreview)
+    }
+
+    private fun startCamera(viewFinder: PreviewView) {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
+                .build()
+                .also {
+//                    it.setAnalyzer(cameraExecutor) { imageProxy ->
+//                        proxyProgress(imageProxy)
+//                    }
+                }
+
+            cameraProvider.bindToLifecycle(
+                viewLifecycleOwner,
+                cameraSelector,
+                preview,
+                imageAnalysis
+            )
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+    private fun permissionHandler() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestCameraPermission()
+        } else {
+        }
+    }
+
+    private fun requestCameraPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission granted, handle the logic for accessing the camera
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Camera permission denied",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
 }
--- app/src/main/java/com/samra/facedetection/ui/result/ResultFragment.kt
+++ app/src/main/java/com/samra/facedetection/ui/result/ResultFragment.kt
@@ -6,15 +6,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.samra.facedetection.databinding.FragmentResultBinding
-
 
 class ResultFragment : Fragment() {
     private lateinit var binding: FragmentResultBinding
     private val resultAdapter: ResultAdapter by lazy { ResultAdapter() }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,25 +31,14 @@ class ResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.recyclerView.adapter = resultAdapter
+        binding.plusBtn.setOnClickListener {
+            findNavController().navigate(ResultFragmentDirections.actionResultFragmentToCameraFragment())
+        }
     }
 
     fun observeResponse() {
 
     }
 
-    fun permissionHandler() {
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.CAMERA),
-                1001
-            )
-        }else{
 
-        }
-    }
 }
